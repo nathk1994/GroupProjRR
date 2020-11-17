133fe4c1fb73ce3d847f254d1855cb46ea7e94fa
Richard Bermingham <richard.bermingham@mycit.ie>
2020-11-17 15:51:50 +0000

    Fourth commit - editing HelloPython.py - Jenkins test

--- HelloPython.py
+++ HelloPython.py
@@ -3,7 +3,7 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
-
+print ("This is a jenkins test")
 #First test comment - testing codeship (changing file)#
 
 	
