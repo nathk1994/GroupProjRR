7efca874553848dc9febef16313f9c7a704631cd
Nathan Keogh <Nathan.keogh@mycit.ie>
2020-11-24 15:40:38 +0000

    7th Commit - edited python file - To test Jenkins Webhook in lab again, switching server

--- HelloPython.py
+++ HelloPython.py
@@ -9,4 +9,7 @@ print ("This is a jenkins test")
 
 #2nd test comment - To test Jenkins In lab#
 
-#3rd test comment - testing Jenkins again in lab after editing jenkins config for webhook. (changing file)#
+#3rd test comment - testing Jenkins again in lab after editing jenkins config for webhook. #
+
+#4th test comment - testing Jenkins again in lab after editing jenkins config for webhook. )#
+
