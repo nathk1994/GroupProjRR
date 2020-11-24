ee31ee4c84c6a213d6ca3e5e7dea20d8f4e86655
Nathan Keogh <Nathan.keogh@mycit.ie>
2020-11-24 15:13:23 +0000

    Sixth Commit - edited python file - To test Jenkins Webhook in lab again after more config changes

--- HelloPython.py
+++ HelloPython.py
@@ -4,8 +4,9 @@ now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 print ("This is a jenkins test")
+
 #First test comment - testing codeship (changing file)#
 
 #2nd test comment - To test Jenkins In lab#
 
-	
+#3rd test comment - testing Jenkins again in lab after editing jenkins config for webhook. (changing file)#
