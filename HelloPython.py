dde7a7b3d18b9f67969ffc22fa86cd0bee0007f6
Nathan Keogh <Nathan.keogh@mycit.ie>
2020-12-01 14:51:09 +0000

    13th Commit - edited python file - Test build, for demo.

--- HelloPython.py
+++ HelloPython.py
@@ -9,10 +9,10 @@ print ("This is a jenkins test")
 
 #2nd test comment - To test Jenkins In lab#
 
-#3rd test comment - testing Jenkins again in lab after editing jenkins config for webhook. #
+#3rd test comment - testing Jenkins again in lab after editing jenkins config for webhook.#
 
-#4th test comment - testing Jenkins again in lab after editing jenkins config for webhook. )#
-#5th test#
+#4th test comment - testing Jenkins again in lab after editing jenkins config for webhook.#
 
-##5th test comment - test build, before a possbiel demo##
+##5th test comment - test build, before a possible demo##
 
+#6th test comment - test build, for demonstration. #
